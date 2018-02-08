5db74d2da1706073178d51dfc3ba804e4224a670
wmsegar <wayne.segar@gmail.com>
2018-02-08 16:35:01 -0500

    added mesos-master as detected PG

--- mesos_plugin.py
+++ mesos_plugin.py
@@ -3,6 +3,7 @@ import json
 import logging
 from ruxit.api.base_plugin import BasePlugin
 from ruxit.api.snapshot import pgi_name
+from ruxit.api.exceptions import ConfigException
 
 class DemoPlugin(BasePlugin):
     def initialize(self, **kwargs):
@@ -16,21 +17,31 @@ class DemoPlugin(BasePlugin):
         domain = config["domain"]
         port = config["port"]
         uri = config["uri"]
+        pgi = None
+        try:
+            pgi = self.find_single_process_group(pgi_name('mesos-master'))
+        except ConfigException:
+            try:
+                pgi = self.find_single_process_group(pgi_name('mesosphere.marathon.Main'))
+            except ConfigException:
+                pass
 
-        pgi = self.find_single_process_group(pgi_name('mesosphere.marathon.Main'))
-        pgi_id = pgi.group_instance_id
-        stats_url = ("http://"+domain+":"+port+uri)
-        #stats_url = ("http://demo1693188.mockable.io/")
-        stats = json.loads(requests.get(stats_url, auth=(user, password)).content.decode())
 
-        for metric in self.metrics:
-            stat_key = metric["source"]["stat"]
-            print(stat_key)
-            stat_value = stats[stat_key]
-            metric_key = metric["timeseries"]["key"]
 
-            self.results_builder.absolute(
-                key = metric_key,
-                value = stat_value,
-                entity_id = pgi_id
-            )
+
+        if pgi is not None:
+            pgi_id = pgi.group_instance_id
+            stats_url = ("http://"+domain+":"+port+uri)
+            stats = json.loads(requests.get(stats_url, auth=(user, password)).content.decode())
+
+            for metric in self.metrics:
+                stat_key = metric["source"]["stat"]
+                print(stat_key)
+                stat_value = stats[stat_key]
+                metric_key = metric["timeseries"]["key"]
+
+                self.results_builder.absolute(
+                    key = metric_key,
+                    value = stat_value,
+                    entity_id = pgi_id
+                    )
